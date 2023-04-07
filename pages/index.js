05267c1fa5fbc60cbb580949307eac394ec93952
unknown <92244268+erthanest@users.noreply.github.com>
2023-04-07 20:09:15 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,66 @@
-// at the top of your component file
+import Head from 'next/head'
 
 import { fetchEntries } from '@utils/contentfulPosts'
+
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 import Post from '@components/Post'
 
-// inside your component markup, pull `posts` from props
+export default function Home({ posts }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next + Contentful Starter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-<div className="posts">
-  {posts.map((p) => {
-    return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-  })}
-</div>
+      <main>
+        <Header />
+        <div className="posts">
+          {posts.map((p) => {
+            return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+          })}
+        </div>
+      </main>
 
-// at the bottom of your component file
+      <Footer />
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .posts {
+          display: flex;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export async function getStaticProps() {
   const res = await fetchEntries()
